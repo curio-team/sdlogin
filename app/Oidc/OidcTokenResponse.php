b942a26e504120cee7dc962b11f3a94e8ca729fb
Bart Roos <bart@studiokaa.co>
2017-10-15 16:50:20 +0200

    Fix9

--- app/Oidc/OidcTokenResponse.php
+++ app/Oidc/OidcTokenResponse.php
@@ -16,47 +16,10 @@ use League\OAuth2\Server\Entities\AccessTokenEntityInterface;
 use League\OAuth2\Server\Entities\RefreshTokenEntityInterface;
 use Psr\Http\Message\ResponseInterface;
 
-class OidcTokenResponse extends \League\OAuth2\Server\ResponseTypes\AbstractResponseType
+class OidcTokenResponse extends \League\OAuth2\Server\ResponseTypes\BearerTokenResponse
 {
-
-    public function generateHttpResponse(ResponseInterface $response)
+    protected function getExtraParams(AccessTokenEntityInterface $accessToken)
     {
-        $expireDateTime = $this->accessToken->getExpiryDateTime()->getTimestamp();
-
-        $jwtAccessToken = $this->accessToken->convertToJWT($this->privateKey);
-
-        $responseParams = [
-            'token_type'   => 'Bearer',
-            'expires_in'   => $expireDateTime - (new \DateTime())->getTimestamp(),
-            'access_token' => (string) $jwtAccessToken,
-            'blaat'        => 'blub',
-        ];
-
-        if ($this->refreshToken instanceof RefreshTokenEntityInterface) {
-            $refreshToken = $this->encrypt(
-                json_encode(
-                    [
-                        'client_id'        => $this->accessToken->getClient()->getIdentifier(),
-                        'refresh_token_id' => $this->refreshToken->getIdentifier(),
-                        'access_token_id'  => $this->accessToken->getIdentifier(),
-                        'scopes'           => $this->accessToken->getScopes(),
-                        'user_id'          => $this->accessToken->getUserIdentifier(),
-                        'expire_time'      => $this->refreshToken->getExpiryDateTime()->getTimestamp(),
-                    ]
-                )
-            );
-
-            $responseParams['refresh_token'] = $refreshToken;
-        }
-
-        $response = $response
-            ->withStatus(200)
-            ->withHeader('pragma', 'no-cache')
-            ->withHeader('cache-control', 'no-store')
-            ->withHeader('content-type', 'application/json; charset=UTF-8');
-
-        $response->getBody()->write(json_encode($responseParams));
-
-        return $response;
+        return array('blaat'        => 'blub');
     }
 }
