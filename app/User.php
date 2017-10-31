d16b040902836750b51edea1cca6b7dcee025d88
Bart Roos <bart@studiokaa.co>
2017-10-31 21:59:23 +0100

    Fix26

--- app/User.php
+++ app/User.php
@@ -33,7 +33,7 @@ class User extends Authenticatable
 
     public function sendPasswordResetNotification($token)
     {
-        $this->notify(new \App\Notifications\MyResetPasswordNotification($token, $user));
+        $this->notify(new \App\Notifications\MyResetPasswordNotification($token, $this));
     }
 
     public function groups()
