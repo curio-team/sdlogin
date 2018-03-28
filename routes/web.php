9f919f0e177f2d4b902f29453b214d3adea80663
Bart Roos <bart@studiokaa.co>
2018-03-28 11:53:48 +0200

    Put every route in a group

--- routes/web.php
+++ routes/web.php
@@ -13,45 +13,53 @@
 
 Route::group(['domain' => 'testlink.amo.rocks'], function() {
     
+    Route::get('/', function() {
+    	return redirect('https://login.amo.rocks/');
+    })->name('home');
+
     Route::get('blaat', function() {
         return 'blaatblub';
     });
 
-})
-
-Route::group(['middleware' => 'auth'], function() {
-	
-	Route::redirect('/', '/me', 301);
-	Route::redirect('/home', '/me', 301);
-	Route::get('/me', 'DashboardController@show')->name('home');
-	Route::get('/users/{user}/profile', 'UserController@profile');
-	Route::patch('/users/{user}/profile', 'UserController@profile_update');
-
-	Route::group(['middleware' => 'admin'], function() {
-		
-		Route::resource('clients', 'MyClientController', ['except' => ['edit', 'update']]);
-		Route::get('clients/{client}/toggle', 'MyClientController@toggle_dev');
-		
-		Route::get('groups/create/batch', 'BatchGroupController@create');
-		Route::post('groups/batch', 'BatchGroupController@store');
-		Route::delete('/groups', 'GroupController@destroy');
-		Route::resource('groups', 'GroupController', ['except' => ['show', 'destroy']]);
-
-		Route::post('/users/import', 'ImportController@upload');
-		Route::get('/users/import', 'ImportController@show');
-		Route::delete('/users', 'UserController@destroy');
-		Route::resource('users', 'UserController', ['except' => ['show', 'destroy']]);
-		
-	});
 });
 
-Route::view('passwords', 'auth.passwords');
-Route::get('login', '\App\Http\Controllers\Auth\LoginController@showLoginForm')->name('login');
-Route::post('login', '\App\Http\Controllers\Auth\LoginController@login');
-Route::get('logout', '\App\Http\Controllers\Auth\LoginController@logout')->name('logout');
+Route::group(['domain' => 'login.amo.rocks'], function() {
 
-// Password Reset Routes...
-Route::get('password/reset', '\App\Http\Controllers\Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
-Route::post('password/email', '\App\Http\Controllers\Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
-Route::get('password/reset/{token}', '\App\Http\Controllers\Auth\ResetPasswordController@showResetForm')->name('password.reset');
-Route::post('password/reset', '\App\Http\Controllers\Auth\ResetPasswordController@reset');
+	Route::group(['middleware' => 'auth'], function() {
+		
+		Route::redirect('/', '/me', 301);
+		Route::redirect('/home', '/me', 301);
+		Route::get('/me', 'DashboardController@show')->name('home');
+		Route::get('/users/{user}/profile', 'UserController@profile');
+		Route::patch('/users/{user}/profile', 'UserController@profile_update');
+
+		Route::group(['middleware' => 'admin'], function() {
+			
+			Route::resource('clients', 'MyClientController', ['except' => ['edit', 'update']]);
+			Route::get('clients/{client}/toggle', 'MyClientController@toggle_dev');
+			
+			Route::get('groups/create/batch', 'BatchGroupController@create');
+			Route::post('groups/batch', 'BatchGroupController@store');
+			Route::delete('/groups', 'GroupController@destroy');
+			Route::resource('groups', 'GroupController', ['except' => ['show', 'destroy']]);
+
+			Route::post('/users/import', 'ImportController@upload');
+			Route::get('/users/import', 'ImportController@show');
+			Route::delete('/users', 'UserController@destroy');
+			Route::resource('users', 'UserController', ['except' => ['show', 'destroy']]);
+			
+		});
+	});
+
+	Route::view('passwords', 'auth.passwords');
+	Route::get('login', '\App\Http\Controllers\Auth\LoginController@showLoginForm')->name('login');
+	Route::post('login', '\App\Http\Controllers\Auth\LoginController@login');
+	Route::get('logout', '\App\Http\Controllers\Auth\LoginController@logout')->name('logout');
+
+	// Password Reset Routes...
+	Route::get('password/reset', '\App\Http\Controllers\Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
+	Route::post('password/email', '\App\Http\Controllers\Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
+	Route::get('password/reset/{token}', '\App\Http\Controllers\Auth\ResetPasswordController@showResetForm')->name('password.reset');
+	Route::post('password/reset', '\App\Http\Controllers\Auth\ResetPasswordController@reset');
+
+});
