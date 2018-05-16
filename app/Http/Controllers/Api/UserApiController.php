fad34332c7effbd15fa2952504c03592d90504f8
Bart Roos <bart@studiokaa.co>
2018-05-16 22:17:27 +0200

    Fix 'me' endpoint

--- app/Http/Controllers/Api/UserApiController.php
+++ app/Http/Controllers/Api/UserApiController.php
@@ -10,6 +10,6 @@ class UserApiController extends Controller
 {
     public function me()
     {
-        return Auth::user()->with('groups');
+        return Auth::user()->with('groups')->get();
     }
 }
