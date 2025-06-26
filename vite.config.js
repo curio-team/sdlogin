f973e7eec6182d3a0eb154842aeb41aefb7b7098
luttje <2738114+luttje@users.noreply.github.com>
2025-06-26 12:46:10 +0200

    Add shared.csss to vite manifest

--- vite.config.js
+++ vite.config.js
@@ -9,6 +9,7 @@ export default defineConfig({
                 'resources/js/app.js',
 
                 'resources/css/login.css',
+                'resources/css/shared.css',
             ],
             refresh: true,
         }),
