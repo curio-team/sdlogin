a287f3ebb0e5d286b4e15e4e53716e997aa4c905
luttje <2738114+luttje@users.noreply.github.com>
2026-05-30 17:36:23 +0200

    Fix checkbox not functioning when clicked directly

--- public/js/check.js
+++ public/js/check.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
   $('#check-table tbody tr').click(function(event) {
-    if (event.target.type !== 'checkbox') {
+    if (event.target.type !== 'checkbox' && !$(event.target).closest('.checkbox-wrapper').length) {
       $(':checkbox', this).trigger('click');
     }
   });
